eb3dc0bfda4bb3989234f21ddb38931f1f3c3b01
William <orangetnt827@gmail.com>
2024-09-11 14:37:37 -0500

    abnihav

---
+++ uil/22-23/invite-a/Abhinav.java
@@ -0,0 +1,7 @@
+public class Abhinav {
+  public static void main(String[] args) {
+    System.out.println("Ultimate Important Languages");
+    System.out.println("U-I-L");
+    System.out.println("UIL!");
+  } 
+}
